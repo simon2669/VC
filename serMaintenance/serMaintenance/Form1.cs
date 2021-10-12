df5e500a26b89c7cc417ca56785256249a103d7b
simon2669 <feltalalo26@gmail.com>
2021-10-12 20:13:27 +0200

    c

--- serMaintenance/serMaintenance/Form1.cs
+++ serMaintenance/serMaintenance/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,6 +38,28 @@ namespace serMaintenance
                 FullName = textBox1.Text + " " + textBox2.Text
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using(var sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                sfd.FilterIndex = 2;
+
+                if(sfd.ShowDialog() == DialogResult.OK)
+                {
+                    TextWriter tw = new StreamWriter(sfd.FileName);
+
+                    foreach(User s in users)
+                    {
+                        tw.WriteLine(s.FullName+";");
+
+
+                    }
+                    tw.Close();
+                }
+            }
         }
     }
 }
