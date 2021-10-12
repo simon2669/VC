ae6f4599cbc56fdc5c513671c51a99359eb730d6
simon2669 <feltalalo26@gmail.com>
2021-10-12 18:18:59 +0200

    initial

---
+++ serMaintenance/serMaintenance/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace serMaintenance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = Resource1.LastName; // label1
+            label2.Text = Resource1.FirstName; // label2
+            button1.Text = Resource1.Add; // button1
+        }
+    }
+}
